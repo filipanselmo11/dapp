fad85cb397b8974f96b8d7412132add0cb513e2c
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-03-12 16:20:36 -0400

    Configurando o drizzle para a rede de teste kovan

--- app/src/drizzleOptions.ts
+++ app/src/drizzleOptions.ts
@@ -5,7 +5,7 @@ const options = {
         block: false,
         fallback: {
             type: 'ws',
-            url: 'ws://127.0.0.1:8545'
+            url: 'wss://kovan.infura.io/ws/v3/85251234652d4e36b814c3f61c179f1c'
         }
     },
     contracts: [ExameMedico],

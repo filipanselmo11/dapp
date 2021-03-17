c9af15dd3382a2fc2c8cbcf509a411453552a660
Filip Anselmo <filip_anselmo@yahoo.com.br>
2021-03-17 06:39:27 -0400

    adicionando o infura.io

--- app/src/store/ipfs.ts
+++ app/src/store/ipfs.ts
@@ -1,5 +1,5 @@
 import IPFS from 'ipfs-http-client';
 
-const ipfs = new IPFS('http://localhost:5001');
-
+//const ipfs = new IPFS('http://localhost:5001');
+const ipfs = new IPFS('ipfs.infura.io:5001');
 export default ipfs;
